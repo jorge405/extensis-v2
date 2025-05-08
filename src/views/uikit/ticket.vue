5476468dffee03e2f11e2ed9f43955e586ab6195
jorge405 <jorgepomsjhope7@gmail.com>
2025-05-08 14:27:31 -0400

    modificando la lista de ticket v2

--- src/views/uikit/ticket.vue
+++ src/views/uikit/ticket.vue
@@ -3,10 +3,12 @@ import axios from 'axios';
 export default{
     data(){
         return{
-        dropdownItems:[
-            { name: 'Option 1', code: 'Option 1' },
-            { name: 'Option 2', code: 'Option 2' },
-            { name: 'Option 3', code: 'Option 3' }
+        
+        estado_tik_options:[
+            {name:'cerrado C.S',code:'option 1'},
+            {name:'postergado',code:'option 2'},
+            {name:'en proceso',code:'option 3'},
+            {name:'cerrado S.S',code:'option 4'},
         ],
         ticket:null,   
         }
@@ -23,8 +25,11 @@ export default{
             } catch (error) {
                 console.log(error);
             }
-
-        }
+        },
+        toggleDropdown(ticketId) {
+            // Alternar la visibilidad del dropdown para la fila específica
+            this.$set(this.dropdownVisible, ticketId, !this.dropdownVisible[ticketId]);
+        },
     }
 }
 
@@ -35,7 +40,7 @@ export default{
 <div class="card flex flex-col gap-4 w-full">
     <div class="w-1/5 flex flex-col gap-4">
         <label for="estado" class=" font-semibold text-xl">Estado:</label>
-        <Select id="estado"  :options="dropdownItems" optionLabel="name" placeholder="Selecciona" class="w-full"></Select>
+        <Select id="estado"  :options="estado_tik_options" optionLabel="name" placeholder="Selecciona" class="w-full"></Select>
     </div>
     <div class=" flex-grow"></div>
     <div class="font-semibold text-xl mb-4">Lista Tickets</div>
@@ -56,18 +61,28 @@ export default{
                         <InputIcon>
                             <i class="pi pi-search" />
                         </InputIcon>
-                        <InputText  placeholder="Keyword Search" />
+                        <InputText  placeholder="Busqueda" />
                     </IconField>
                 </div>
             </template>
             <template #empty> No existe tickets . </template>
             <template #loading> cargando datos tickets. Porfavor espere. </template>
-            <Column field="codigo ticket" header="codigo ticket" style="min-width: 12rem">
+            <Column header="Estado"  style="min-width: 14rem">
                 <template #body="{ data }">
-                    {{ data.cod_ticket }}
+                    <div class="items-center gap-2">
+                        <div class=" rounded-md p-2 text-white w-1/2" :class=" data.estado_tik ==='cerrado S.S' ? 'bg-orange-500' : 'bg-sky-600'">{{ data.estado_tik }}</div>
+                        <p class=" text-sm">Ticket: {{ data.cod_ticket }}</p>
+                    </div>
                 </template>
                 <!--<template #filter="{ filterModel }">
-                    <InputText v-model="filterModel.value" type="text" placeholder="Search by name" />
+                    <MultiSelect v-model="filterModel.value" :options="representatives" optionLabel="name" placeholder="Any">
+                        <template #option="slotProps">
+                            <div class="flex items-center gap-2">
+                                <img :alt="slotProps.option.name" :src="`https://primefaces.org/cdn/primevue/images/avatar/${slotProps.option.image}`" style="width: 32px" />
+                                <span>{{ slotProps.option.name }}</span>
+                            </div>
+                        </template>
+                    </MultiSelect>
                 </template>-->
             </Column>
             <Column header="PDV N Fantasia "  style="min-width: 12rem">
@@ -88,25 +103,24 @@ export default{
                     <Button type="button" icon="pi pi-check" @click="filterCallback()" severity="success"></Button>
                 </template>-->
             </Column>
-            <Column header="Estado"  style="min-width: 14rem">
+            <Column header="RG"  style="min-width: 12rem">
                 <template #body="{ data }">
-                    <div class="items-center gap-2">
-                        <div class=" bg-orange-500 rounded-md p-2 text-white w-1/2">{{ data.estado_tik }}</div>
-                        <p class=" text-sm">Ticket: {{ data.cod_ticket }}</p>
+                    <div class="flex items-center gap-2">
+                        <span>{{ data.COD_REG }}</span>
+                        
                     </div>
                 </template>
                 <!--<template #filter="{ filterModel }">
-                    <MultiSelect v-model="filterModel.value" :options="representatives" optionLabel="name" placeholder="Any">
-                        <template #option="slotProps">
-                            <div class="flex items-center gap-2">
-                                <img :alt="slotProps.option.name" :src="`https://primefaces.org/cdn/primevue/images/avatar/${slotProps.option.image}`" style="width: 32px" />
-                                <span>{{ slotProps.option.name }}</span>
-                            </div>
-                        </template>
-                    </MultiSelect>
+                    <InputText v-model="filterModel.value" type="text" placeholder="Search by country" />
+                </template>
+                <template #filterclear="{ filterCallback }">
+                    <Button type="button" icon="pi pi-times" @click="filterCallback()" severity="secondary"></Button>
+                </template>
+                <template #filterapply="{ filterCallback }">
+                    <Button type="button" icon="pi pi-check" @click="filterCallback()" severity="success"></Button>
                 </template>-->
             </Column>
-            <Column header="Solicitante"  style="min-width: 14rem">
+            <Column header="Remitente"  style="min-width: 14rem">
                 <template #body="">
                     <div class="flex items-center gap-2">
                         <span></span>
@@ -126,7 +140,7 @@ export default{
             <Column header="Operacion"  style="min-width: 12rem">
                 <template #body="{ data }">
                     <div class=" items-center gap-2">
-                        <div class="w-full "> <span class=" font-bold">Solicitado:</span> {{ data.solicitada }}</div>
+                        <div class="w-full "> <span class=" font-bold">Solicitado:</span> {{ data.op_solicitada }}</div>
                         <p class="w-full "><span class=" font-bold">Realizado:</span> {{ data.op_realizada }}</p>
                     </div>
                 </template>
@@ -176,6 +190,7 @@ export default{
                 <template #body="">
                     <div class="flex items-center gap-2">
                         <button class=" bg-slate-700 p-4 rounded-md">Detalles <i class="pi pi-window-maximize"></i></button>
+                        
                     </div>
                 </template>
                 <!--<template #filter="{ filterModel }">
@@ -188,10 +203,43 @@ export default{
                     <Button type="button" icon="pi pi-check" @click="filterCallback()" severity="success"></Button>
                 </template>-->
             </Column>
+
             <Column header="Aprobado"  style="min-width: 12rem">
                 <template #body="{ data }">
                     <div class="flex items-center gap-2">
                         <span>{{ data.fecha_asignada }}</span>
+                    </div>
+                </template>
+                <!--<template #filter="{ filterModel }">
+                    <InputText v-model="filterModel.value" type="text" placeholder="Search by country" />
+                </template>
+                <template #filterclear="{ filterCallback }">
+                    <Button type="button" icon="pi pi-times" @click="filterCallback()" severity="secondary"></Button>
+                </template>
+                <template #filterapply="{ filterCallback }">
+                    <Button type="button" icon="pi pi-check" @click="filterCallback()" severity="success"></Button>
+                </template>-->
+            </Column>
+            <Column header="Transcurrido"  style="min-width: 12rem">
+                <template #body="">
+                    <div class="flex items-center gap-2">
+                        <span></span>
+                    </div>
+                </template>
+                <!--<template #filter="{ filterModel }">
+                    <InputText v-model="filterModel.value" type="text" placeholder="Search by country" />
+                </template>
+                <template #filterclear="{ filterCallback }">
+                    <Button type="button" icon="pi pi-times" @click="filterCallback()" severity="secondary"></Button>
+                </template>
+                <template #filterapply="{ filterCallback }">
+                    <Button type="button" icon="pi pi-check" @click="filterCallback()" severity="success"></Button>
+                </template>-->
+            </Column>
+            <Column header="Atencion aprox."  style="min-width: 12rem">
+                <template #body="">
+                    <div class="flex items-center gap-2">
+                        <span></span>
                     </div>
                 </template>
                 <!--<template #filter="{ filterModel }">
